32d62c98519cd0509b5168ff7f52494c30adc88e
licj <396638509@qq.com>
2018-03-04 16:02:11 +0800

    mac

--- helloworld/main.c
+++ helloworld/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include "render.h"
+
+const char *vshader = "#version 410\n"
+		"in vec3 vp;"
+		"void main () {"
+		"  gl_Position = vec4 (vp, 1.0);"
+		"}";
+const char *fshader = "#version 410\n"
+		"out vec4 frag_colour;"
+		"void main () {"
+		"  frag_colour = vec4 (0.5, 0.2, 0.3, 1.0);"
+		"}";
+GLfloat points[] = {-1.0f, -0.5f, 0.0f, 1.0f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f};
 
 int main(void)
 {
@@ -20,11 +31,48 @@ int main(void)
         return -1;
     }
     glfwMakeContextCurrent(window);
-    
+
+	glewInit();
     glViewport(0, 0, 800, 600);
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo); 
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GL_FLOAT), points, GL_STATIC_DRAW);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glBindVertexArray(0);
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vshader, NULL);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fshader, NULL);
+    glCompileShader(fs);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
     while(!glfwWindowShouldClose(window))
     {
-        Render();
+        glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
--- helloworld/render.c
+++
@@ -1,8 +0,0 @@
-#include <GL/glew.h>
-#include "render.h"
-
-void Render()
-{
-    glClearColor(0.2f, 0.3f, 0.5f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-}
